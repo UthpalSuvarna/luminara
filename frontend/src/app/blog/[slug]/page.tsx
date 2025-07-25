9e4b3887a6ab4015f66df02d6673100064e221fe
UthpalSuvarna <uthpal33@gmail.com>
2025-07-25 23:15:22 +0530

    Fix the type of param

--- frontend/src/app/blog/[slug]/page.tsx
+++ frontend/src/app/blog/[slug]/page.tsx
@@ -1,10 +1,15 @@
 import fetchBlog from "@/helpers/getBlog";
 import RichTextRenderer from "./RichTextRenderer";
-export default async function Blog({ params }: { params: { slug: string } }) {
+
+export default async function Blog({
+    params,
+}: {
+    params: Promise<{ slug: string }>;
+}) {
     const { slug } = await params;
     const blog = await fetchBlog(slug);
     const content = blog.data[0].Content;
-    console.log(content[2])
+
     return (
         <div>
             <p>{blog.data[0].Title}</p>
