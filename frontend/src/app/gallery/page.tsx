1af4c3d5edb2f3a3021ee99db2d45bd507929972
thejasbshetty <thejasbshetty@gmail.com>
2025-08-28 23:09:37 +0530

    Add Header component to gallery page layout

--- frontend/src/app/gallery/page.tsx
+++ frontend/src/app/gallery/page.tsx
@@ -1,9 +1,12 @@
 import GalleryPage from "@/components/gallery-page"
 import { Footer } from '@/components/footer'
+import { Header } from '@/components/header'
+  
 
 export default function Home() {
   return (
     <>
+      <Header />
       <GalleryPage />
       <Footer />
     </>
